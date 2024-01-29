111e73468bfe42b9313133036d08abad6f0665ec
HeroBuxx <me@herobuxx.me>
2024-01-29 10:25:37 -0800

    Add Insert Order data
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ TB-P2PBO/TB-P2PBO/model/M_orders.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB_P2PBO.model
+{
+    internal class M_orders
+
+    {
+        string id, idevent, nama, email, gender;
+
+        public M_orders()
+        {
+
+        }
+
+        public M_orders(string id, string idevent, string nama, string email, string gender)
+        {
+            this.ID = id;
+            this.IDEvent = idevent;
+            this.Nama = nama;
+            this.Email = email;
+            this.Gender = gender;
+        }
+
+        public string ID { get => id; set => id = value; }
+        public string IDEvent { get => idevent; set => idevent = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public string Email { get => email; set => email = value; }
+        public string Gender { get => gender; set => gender = value; }
+    }
+}
--- TB-P2PBO/TB-P2PBO/view/Rnb.cs
+++ TB-P2PBO/TB-P2PBO/view/Rnb.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,12 +10,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TB_P2PBO.controller;
+using TB_P2PBO.model;
 
 namespace TB_P2PBO.view
 {
     public partial class Rnb : Form
     {
+        public string ID { get; set; }
+
         private Koneksi koneksi = new Koneksi();
+        M_orders m_orders = new M_orders();
 
         public Rnb()
         {
@@ -28,7 +33,6 @@ namespace TB_P2PBO.view
 
         private void btn_simpanRnb_Click(object sender, EventArgs e)
         {
-
             // Validasi input
             if (cb_Rnb.SelectedIndex == -1)
             {
@@ -54,8 +58,18 @@ namespace TB_P2PBO.view
                 return;
             }
 
-            // Jika semua validasi berhasil, tampilkan pesan pembelian berhasil
+            orders orders = new orders();
+            m_orders.ID = Guid.NewGuid().ToString();
+            m_orders.Nama = tb_namaRnb.Text;
+            m_orders.Email = tb_emailRnb.Text;
+
+
             string jenisKelamin = rb_rnbL.Checked ? "Laki-Laki" : "Perempuan";
+            m_orders.Gender = jenisKelamin;
+
+            orders.InsertOrder(m_orders);
+
+            // Jika semua validasi berhasil, tampilkan pesan pembelian berhasil
             string konserTerpilih = cb_Rnb.SelectedItem.ToString();
 
             string pesan = $"Pembelian tiket berhasil!, Check Email\n\n" +
@@ -65,6 +79,7 @@ namespace TB_P2PBO.view
                            $"Jenis Kelamin: {jenisKelamin}";
 
             MessageBox.Show(pesan, "Pembelian Tiket", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
 
         private void Rnb_Load(object sender, EventArgs e)
@@ -78,20 +93,23 @@ namespace TB_P2PBO.view
             try
             {
                 koneksi.OpenConnection();
-                string query = "SELECT artist, title, price, location, date FROM events WHERE genre='RNB'";
+                string query = "SELECT id, artist, title, price, location, date FROM events WHERE genre='RNB'";
                 MySqlCommand cmd = new MySqlCommand(query, koneksi.kon);
                 using (MySqlDataReader reader = cmd.ExecuteReader())
                 {
                     cb_Rnb.Items.Clear();
                     while (reader.Read())
                     {
+                        string id = reader["id"].ToString();
                         string artist = reader["artist"].ToString();
                         string title = reader["title"].ToString();
                         string price = reader["price"].ToString();
                         string location = reader["location"].ToString();
                         string date = reader["date"].ToString();
 
-                        cb_Rnb.Items.Add(" [" + date + "] - " + artist + "  -" + title + " - USD " + price + " - " + location);
+                        // Include the ID in the ComboBox items using the Tag property
+                        string displayText = $" [{date}] - {artist} - {title} - USD {price} - {location}";
+                        cb_Rnb.Items.Add(new ComboboxItem(displayText, id));
                     }
                 }
             }
@@ -102,6 +120,18 @@ namespace TB_P2PBO.view
             finally
             {
                 koneksi.CloseConnection();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void cb_Rnb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cb_Rnb.SelectedItem is ComboboxItem selectedItem)
+            {
+                m_orders.IDEvent = selectedItem.Value;
             }
         }
     }
